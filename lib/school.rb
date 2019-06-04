523c2c78b462151b2f0a4a7435ac166d218fcdd0
kevin lang <langkevina@gmail.com>
2019-06-04 00:17:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  roster = {}
+
+  def initialize(schoolName)
+    @schoolName = schoolName
+  end
+
+  def add_student(name, vKey)
+    roster.each do |key|
+      if key == vKey
+        roster[key] << name
+      elsif
+        roster[key] = vKey      
+      end
+    end
+  end
+
+  def grade(grade)
+
+  end
+
+  def sort
+
+  end
+
+end
